367e1551e66fd8693821c72b13a6e1c3faad5c17
Christian <christianfryksten1@gmail.com>
2025-05-06 16:16:21 +0200

    Add header

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,6 +5,7 @@ export default function RegisterPage() {
   return (
     <>
       <Header />
+      <h1 className="text-primary text-center bg-dark p-3 m-0">Bli medlem på Kino Bio</h1>
       <RegisterForm />;
     </>
   );
