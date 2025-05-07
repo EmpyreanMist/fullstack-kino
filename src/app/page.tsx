3794d60e445182e461356c4cca80dce81d94dac6
Kai <kai.ericson@lerniastudent.se>
2025-05-07 15:31:41 +0200

    create component file and make sure it can be shown on the website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '../components/header';
+import HomePage from '@/components/homePage';
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <HomePage />
     </div>
   );
 }
