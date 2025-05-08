cef8eb20bc97c9749696306ba0673ff4ba43653a
Kai <kai.ericson@lerniastudent.se>
2025-05-08 11:25:05 +0200

    add button for all movies and top 5 title

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -1,5 +1,14 @@
+import { Button } from "react-bootstrap";
+import Link from "next/link";
 export default function HomePage(){
     return(
-        <h1>Home Page</h1>
+        <section>
+            <div className="text-center">
+                <Link  href="/movies">
+                    <Button className="fs-5 p-3 " >Alla filmer</Button>
+                </Link>
+            </div>
+            <h2 className="ms-3">Top 5 movies</h2>
+        </section>
     );
 }
