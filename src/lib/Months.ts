e4c2bc9732682c31822328afbd58370f5f6e926f
marcusmoller97 <marcussmoller@icloud.com>
2025-05-11 21:23:51 +0200

    forgot to add types and months so it is done in this commit

---
+++ src/lib/Months.ts
@@ -0,0 +1,29 @@
+export const Months: {
+  [key: string]: string;
+
+  Januari: string;
+  Februari: string;
+  Mars: string;
+  April: string;
+  Maj: string;
+  Juni: string;
+  Juli: string;
+  Augusti: string;
+  September: string;
+  Oktober: string;
+  November: string;
+  December: string;
+} = {
+  Januari: '01',
+  Februari: '02',
+  Mars: '03',
+  April: '04',
+  Maj: '05',
+  Juni: '06',
+  Juli: '07',
+  Augusti: '08',
+  September: '09',
+  Oktober: '10',
+  November: '11',
+  December: '12',
+};
---
+++ src/lib/TypesFormData.ts
@@ -0,0 +1,12 @@
+export interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  birthDay?: number;
+  birthMonth: string;
+  birthYear?: number;
+  city: string;
+  password1: string;
+  comparePassword: string;
+}
