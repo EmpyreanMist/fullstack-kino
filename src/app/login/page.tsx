4036299ed1aa15dd45004cdcbd2f85f1567bf86b
Jan Elia <jan.elia995@hotmail.com>
2025-05-14 00:42:22 +0200

    Deleting the navbar to avoid duplicating with the header

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,6 @@ import Navbar from '../../components/Navbar';
 export default function LoginPage() {
   return (
     <>
-    <Navbar />
       <h1 className="text-primary text-center bg-dark p-3 m-0">Logga in på Kino</h1>
       <LoginForm />
     </>
