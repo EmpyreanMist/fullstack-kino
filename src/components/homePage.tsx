9fd27adcee282e8fc01d981c519da011f3d2553f
Kai <kai.ericson@lerniastudent.se>
2025-05-15 15:54:41 +0200

    clean up the design

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -94,37 +94,35 @@ const screenings: Screening[] = [
             </div>
             <h2 className="text-white m-5 text-center">Top 5 movies</h2>
             <Container className="d-flex flex-wrap justify-content-center">
-            <Col sm={12} md={12} lg={12} xl={10}>
-            <section className="d-flex flex-wrap justify-content-center gap-4 pb-3">
-                {movies.map(movie =>
-                    <CardPoster
-                        img={movie.img}
-                        title={movie.title}
-                        description={movie.description}
-                        width={200}
-                        height={200}
-                        id={movie.id}
-                    />
-                )};
-           </section>
-           </Col>
+                <Col sm={12} md={12} lg={12} xl={10}>
+                <section className="d-flex flex-wrap justify-content-center gap-4 pb-3">
+                    {movies.map(movie =>
+                        <CardPoster
+                            img={movie.img}
+                            title={movie.title}
+                            description={movie.description}
+                            width={200}
+                            height={200}
+                            id={movie.id}
+                        />
+                    )};
+            </section>
+            </Col>
            </Container>
-           <section>
-                <h2 className="text-white m-3 text-center">Kommande visningar</h2>
-                <div className="m-3 pb-3 pt-1 text-center d-flex flex-wrap justify-content-center">
+           <h2 className="text-white m-3 text-center">Kommande visningar</h2>
+                <div className="pb-3 pt-1 text-center d-flex flex-wrap justify-content-center">
                     <Col sm={15} md={10} lg={8} xl={6}>
-                    {screenings.map(screening =>
-                        <UpcomingScreening
-                        title={screening.title}
-                        date={screening.date}
-                        time={screening.time}
-                        room={screening.room}
-                        id={screening.id}
-                         />
-                    )}
+                        {screenings.map(screening =>
+                            <UpcomingScreening
+                            title={screening.title}
+                            date={screening.date}
+                            time={screening.time}
+                            room={screening.room}
+                            id={screening.id}
+                            />
+                        )};
                     </Col>
                 </div>
-           </section>
         </main>
     );
 }
