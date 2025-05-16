4516f333514c6a28c653a2f14a52f34a415984d1
Kai <kai.ericson@lerniastudent.se>
2025-05-16 11:48:09 +0200

    create schema for screening

---
+++ backend/models/screenings.tsx
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const screeningSchema = new mongoose.Schema({
+    id: String,
+    time: String,
+    room: String,
+    movie: {
+        id: String,
+        title: String,
+    },
+});
+
+const Screening = mongoose.model("Screening", screeningSchema);
+export {
+    Screening
+}
