db93a16340a98d3e330bee019a98a5701b89b644
Kai <kai.ericson@lerniastudent.se>
2025-05-22 12:57:08 +0200

    clean up code and finish API route

--- src/app/api/screenings/upcoming/route.ts
+++ src/app/api/screenings/upcoming/route.ts
@@ -1,16 +1,20 @@
+import { NextResponse } from "next/server";
 import connectDB from "../../../../../backend/config/db";
 import { Screening } from "../../../../../backend/models/screenings";
 import sortScreenings from "@/components/ScreeningsSort";
 import { ScreeningProp } from "@/lib/typesScreening";
 
 export async function GET(){
+    let sortedScreenings : ScreeningProp[];
     try{
         connectDB();
         const screenings: ScreeningProp[] = await Screening.find();
         console.log(screenings);
-        const sortedScreenings = sortScreenings(screenings);
+        sortedScreenings = sortScreenings(screenings);
+        return NextResponse.json({data: sortedScreenings})
     } catch(error) {
         console.log("error fetching screenigs")
+        return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
     }
     
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import HomePage from "@/components/homePage";
-import { GET } from "@/app/api/screenings/upcoming/route";
-GET();
+
 export default function Home() {
   return <div>
     <HomePage/>
