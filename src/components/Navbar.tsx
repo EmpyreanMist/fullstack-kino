85ef35ef35d81c43baf7d45a3b94b974e22ec853
Kai <kai.ericson@lerniastudent.se>
2025-05-29 14:31:26 +0200

    update spacing and font size in footer and navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,17 +57,17 @@ export default function Navigation({
   };
 
   return (
-    <Navbar expand="lg" bg="dark" variant="dark" className="p-3 text-light">
+    <Navbar expand="lg" bg="dark" variant="dark" className="p-3 text-light fs-5">
       <Navbar.Toggle aria-controls="navbarScroll" />
       <Navbar.Collapse id="navbarScroll">
         <Nav className="me-auto my-2 my-lg-0" navbarScroll>
-          <Nav.Link as={Link} href="/" className="text-white">
+          <Nav.Link as={Link} href="/" className="text-white pe-4">
             Hem
           </Nav.Link>
-          <Nav.Link as={Link} href="/movies" className="text-white">
+          <Nav.Link as={Link} href="/movies" className="text-white pe-4">
             Alla filmer
           </Nav.Link>
-          <Nav.Link as={Link} href="/about" className="text-white">
+          <Nav.Link as={Link} href="/about" className="text-white pe-4">
             Om oss
           </Nav.Link>
         </Nav>
